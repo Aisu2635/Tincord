8dd942b3ab8b6d490d18cfb3ccf9aa843f5fd888
isitjay <108180623+isitjay@users.noreply.github.com>
2023-08-16 17:02:15 +0530

    Update index.jsx

--- frontend/src/scenes/home/index.jsx
+++ frontend/src/scenes/home/index.jsx
@@ -1,11 +1,33 @@
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
+import { useSelector } from 'react-redux';
 import NavPg from 'scenes/navbar';
 
 
 const HomePg = () => {
-    return <Box>
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    const {_id, picturePath} = useSelector((state) => state.user);
+    return (
+    <Box>
         <NavPg />
-        </Box>;
+        <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent = "space-between"
+        >
+        <Box flexBasis={isNonMobileScreens? "26%" :undefined}>
+            <userWidget userId={_id} picturePath={picturePath}/>
+        </Box>
+        <Box
+        flexBasis={isNonMobileScreens? "42%" :undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+            ></Box>
+        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+    
+        </Box>
+    </Box>
+    );
 };
 
 export default HomePg;
